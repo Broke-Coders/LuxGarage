8e509ba04e1d8f3f752fb7140f1555b6fdec99bc
iSheek <igor.szyk@outlook.com>
2026-03-18 14:51:33 +0100

    add some seeded data, add implementation for migrateDb()

--- LuxGarage.API/Data/DataExtensions.cs
+++ LuxGarage.API/Data/DataExtensions.cs
@@ -8,7 +8,85 @@ public static class DataExtensions
 {
     public static void MigrateDb(this WebApplication app)
     {
-        //TODO implement
+        using var scope = app.Services.CreateScope();
+
+        var dbContext = scope.ServiceProvider
+            .GetRequiredService<RentalContext>();
+
+
+        dbContext.Database.EnsureDeleted(); // FOR RESETING DATABASE!!! 
+
+        dbContext.Database.Migrate();
+
+        if(!dbContext.Set<Workplace>().Any())
+        {
+            new Workplace {Country = "Poland", 
+            City = "Krakow", Street = "Kulszowa", 
+            BuildingNumber = 1};
+
+            new Workplace {Country = "USA", City = "New York", 
+            Street = "Chinastreet", BuildingNumber = 3};
+
+            new Workplace {Country = "Germany", City = "Berlin", 
+            Street = "Hausstradt", BuildingNumber = 12 };
+        }
+
+        if(!dbContext.Set<Worker>().Any())
+        {
+            new Worker {Password = "pass", 
+            WorkplaceId = 1, PermissionId = 1};
+            new Worker {Password = "haslo", 
+            WorkplaceId = 2, PermissionId = 2};
+            new Worker {Password = "1234", 
+            WorkplaceId = 2, PermissionId = 3};
+        }
+
+        if(!dbContext.Set<VehicleBrand>().Any())
+        {
+            new VehicleBrand {Name = "BMW"};
+            new VehicleBrand {Name = "Audi"};
+            new VehicleBrand {Name = "Volkswagen"};
+        }
+
+        if(!dbContext.Set<VehicleBody>().Any())
+        {
+            new VehicleBody {Name = "Sedan"};
+            new VehicleBody {Name = "Van"};
+            new VehicleBody {Name = "Pickup"};
+        }
+
+        if(!dbContext.Set<VehicleColor>().Any())
+        {
+            new VehicleColor{Name = "WHITE", 
+            HtmlColor = "#FFF"};
+            new VehicleColor{Name = "BLACK", 
+            HtmlColor = "#000"};
+            new VehicleColor{Name = "RED", 
+            HtmlColor = "#F00"};
+        }
+
+        if(!dbContext.Set<Insurance>().Any())
+        {
+            new Insurance {Name = "anti-thief",
+            Price = 10.99m};
+            new Insurance {Name = "anti-scratch",
+            Price = 18.99m};
+            new Insurance {Name = "tire-insurance",
+            Price = 6.59m};
+        }
+
+        if(!dbContext.Set<Borrower>().Any())
+        {
+            new Borrower{Email = "wuj@gmail.com",
+            BorrowCounter = 1};
+            new Borrower{Email = "ziutek@outlook.com",
+            BorrowCounter = 5};
+            new Borrower{Email = "hop@maupa.net",
+            BorrowCounter = 3};
+        }
+
+
+
     }
 
     public static void AddStoreDb(this WebApplicationBuilder builder)
