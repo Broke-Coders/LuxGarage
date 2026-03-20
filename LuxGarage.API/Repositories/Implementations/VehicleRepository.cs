9bd6221fbb22526f1379d3b2f3f00a10435c51e1
Maciej Porębski <porebskimacio04@gmail.com>
2026-03-20 15:35:26 +0100

    Add basic vehicle repository methods

--- LuxGarage.API/Repositories/Implementations/VehicleRepository.cs
+++ LuxGarage.API/Repositories/Implementations/VehicleRepository.cs
@@ -1,8 +1,9 @@
 
 using LuxGarage.API.Data;
-using LuxGarage.API.Models;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.EntityFrameworkCore;
+using LuxGarage.API.Models;
+using System.Collections.Immutable;
 
 public class VehicleRepository : IVehicleRepository
 {
@@ -13,9 +14,40 @@ public class VehicleRepository : IVehicleRepository
         this._context = context;
     }
 
-    public async Task<IEnumerable<Vehicle>> GetAllAsync()
-        => await _context.Vehicles.AsNoTracking().ToListAsync();
+    public async Task<IEnumerable<Task>> GetAllAsync()
+        => (IEnumerable<Task>)await _context.Vehicles.AsNoTracking().ToListAsync();
 
     public async Task<Vehicle?> GetByIdAsync(int id)
         => await _context.Vehicles.FindAsync(id);
+
+    public async Task<Vehicle?> GetByLicensePlateAsync(string licensePlate)
+        => await _context.Vehicles.FirstOrDefaultAsync(v => v.LicensePlate == licensePlate);
+    public async Task AddAsync(Vehicle vehicle)
+    {
+        await _context.Vehicles.AddAsync(vehicle);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Vehicle vehicle, int id)
+    {
+        Vehicle? myvehicle = await _context.Vehicles.FindAsync(id);  
+
+        if (myvehicle == null)
+        {
+            Console.WriteLine("Nie ma elementu o podanym id");
+            return;        
+        } 
+
+        myvehicle = vehicle;
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var vehicle = await GetByIdAsync(id);
+        if (vehicle is null) return;
+        _context.Vehicles.Remove(vehicle);
+        await _context.SaveChangesAsync();
+    }
 }
