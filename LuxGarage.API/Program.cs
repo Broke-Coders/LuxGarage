5ad07f888f95de3386f57f300d80f9eddf5c9b14
Maciej Porębski <porebskimacio04@gmail.com>
2026-03-21 17:18:39 +0100

    Add extension for services and repositories

--- LuxGarage.API/Program.cs
+++ LuxGarage.API/Program.cs
@@ -1,8 +1,11 @@
 using LuxGarage.API.Data;
+using LuxGarage.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddStoreDb();
+builder.Services.AddRepositories();
+
 var app = builder.Build();
 app.MigrateDb();
 
