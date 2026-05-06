4fd576b3c2dc3da6e3339cfeabf62bdbb74530b7
Pucdolf <jakub.jh@interia.pl>
2026-05-06 15:31:34 +0200

    Implement VehicleModel entity and integrate it across all API layers

--- LuxGarage.API/DTOs/Requests/CreateVehicleRequest.cs
+++ LuxGarage.API/DTOs/Requests/CreateVehicleRequest.cs
@@ -3,6 +3,7 @@ namespace LuxGarage.API.DTOs.Requests.Vehicle;
 public class CreateVehicleRequest
 {
     public int VehicleBrandId { get; set; }
+    public int VehicleModelId { get; set; }
     public int VehicleBodyId { get; set; }
     public int VehicleColorId { get; set; }
     public decimal Horsepower { get; set; }
---
+++ LuxGarage.API/Repositories/Interfaces/IVehicleModelRepository.cs
@@ -0,0 +1,18 @@
+﻿using LuxGarage.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LuxGarage.API.Repositories.Interfaces
+{
+    public interface IVehicleModelRepository
+    {
+
+        Task<VehicleModel?> GetByIdAsync(int id);
+
+        Task AddAsync(VehicleModel model);
+
+        Task UpdateAsync(VehicleModel model, int id);
+        Task DeleteAsync(int id);
+
+
+    }
+}
