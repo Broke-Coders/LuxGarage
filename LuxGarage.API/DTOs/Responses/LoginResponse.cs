e680f0b7507b5b8bb079401f180ddfd96155b31b
Pucdolf <jakub.jh@interia.pl>
2026-05-06 21:08:27 +0200

    Implement JWT authentication and unify API responses with ApiResponse wrapper

---
+++ LuxGarage.API/DTOs/Responses/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace LuxGarage.API.DTOs.Responses
+{
+    public class LoginResponse
+    {
+        public required string Token { get; set; }
+        public required string Login { get; set; }
+        public required string Role { get; set; }
+    }
+}
