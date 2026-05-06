68fbc16f3d7b4b45fbcf27dbaab8d64d713180d1
Pucdolf <jakub.jh@interia.pl>
2026-05-06 12:48:54 +0200

    Configure Swagger UI and integrate controllers with updated repository names

--- LuxGarage.API/Program.cs
+++ LuxGarage.API/Program.cs
@@ -5,10 +5,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddStoreDb();
 builder.Services.AddRepositories();
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 app.MigrateDb();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 // app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
