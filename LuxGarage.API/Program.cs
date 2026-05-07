66a8b9565e37cd73ffa430ffdbd4c308ebcb3f69
Maciej Porębski <porebskimacio04@gmail.com>
2026-05-07 19:53:26 +0200

    Add car list site

--- LuxGarage.API/Program.cs
+++ LuxGarage.API/Program.cs
@@ -11,6 +11,16 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => {
+
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 app.MigrateDb();
 
