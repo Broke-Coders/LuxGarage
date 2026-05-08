1b8d065806990543dd8d1d066c37a979064b0244
48604 <igor.szyk@outlook.com>
2026-05-08 21:59:30 +0200

    feat: add docs comments

--- LuxGarage.API/DTOs/Requests/GetVehicleRequest.cs
+++ LuxGarage.API/DTOs/Requests/GetVehicleRequest.cs
@@ -1,5 +1,9 @@
 namespace LuxGarage.API.DTOs.Requests.Vehicle;
 
+/// <summary>
+/// DTO for retrieving a list of vehicles, containing optional sorting 
+/// parameters such as the field to sort by and the sort direction (ascending or descending).
+/// </summary>
 public class GetVehiclesRequest
 {
     public string? SortBy { get; set; } = "id";
