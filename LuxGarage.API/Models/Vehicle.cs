c466d44c932bd329ffbc22ab2a4a95822b90ada4
Maciej Porębski <porebskimacio04@gmail.com>
2026-05-08 18:42:49 +0200

    Add offer and image models

--- LuxGarage.API/Models/Vehicle.cs
+++ LuxGarage.API/Models/Vehicle.cs
@@ -6,21 +6,25 @@ public class Vehicle
 {
     public int Id { get; set; }
 
-    public int VehicleBrandId { get; set; } //brand moze do usuniecia??
+    public int VehicleBrandId { get; set; } 
     public VehicleBrand VehicleBrand{ get; set; } = null!;
 
     public int VehicleModelId { get; set; }
     public VehicleModel VehicleModel { get; set; } = null!;
-    public decimal Horsepower { get; set; }
 
+    public int VehicleImageId { get; set; }
+
+    public decimal Horsepower { get; set; }
     public string LicensePlate { get; set; } = null!;
     public int Mileage { get; set; }
-
+    public int year { get; set; }
+    
     public int VehicleBodyId { get; set; }
     public VehicleBody VehicleBody { get; set; } = null!;
 
     public int VehicleColorId { get; set; }
     public VehicleColor VehicleColor { get; set; } = null!;
 
-    public ICollection<Rental> Rentals {get;} = new List<Rental>();
+    public List<Rental> Rentals {get;} = new List<Rental>();
+    public List<VehicleImage> Images { get; set; } = new List<VehicleImage>();
 }
