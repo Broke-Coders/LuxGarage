86eb5d00c348613dda64a0c2ebec28da71839590
48604 <igor.szyk@outlook.com>
2026-05-09 01:44:33 +0200

    feat: add doc comments to js, add js code to whole documentation

--- LuxGarage.Front/wwwroot/js/carService.js
+++ LuxGarage.Front/wwwroot/js/carService.js
@@ -1,7 +1,18 @@
 const API_BASE_URL = 'http://localhost:5054/api';
 
+/***
+ * Get all cars and get car by id services. 
+ * These functions will be used in the CarList and CarDetails components to fetch data from the API.
+ */
+
 export const CarService = {
 
+    /***
+     * Fetches all cars from the API and returns an array of car objects. Each car object contains properties like 
+     * brandName, modelName, year, and horsepower.
+     * @returns dto.data - an array of car objects fetched from the API.
+     * @throws Error - If the API request fails, an error is thrown.
+     */
     async getAllCars() {
         const response = await fetch(`${API_BASE_URL}/Vehicles`);
         if (!response.ok) {
@@ -11,6 +22,13 @@ export const CarService = {
         return dto.data;
     },
 
+        /***
+         * Fetches a single car by its ID from the API and returns a car object. The car object contains properties like
+         * brandName, modelName, year, and horsepower.
+         * @param {number} id - The ID of the car to fetch.
+         * @return dto.data - A car object fetched from the API.
+         * @throws Error - If the API request fails, an error is thrown.
+         */
     async getCarById(id) {
         const response = await fetch(`${API_BASE_URL}/Vehicle/${id}`);
         if (!response.ok) {
