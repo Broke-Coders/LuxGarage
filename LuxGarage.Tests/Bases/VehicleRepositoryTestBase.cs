5bc8ffcaecb2c526ccef272851b7b1f23bd42896
48604 <igor.szyk@outlook.com>
2026-05-08 14:53:05 +0200

    test: change existing tests

--- LuxGarage.Tests/Bases/VehicleRepositoryTestBase.cs
+++ LuxGarage.Tests/Bases/VehicleRepositoryTestBase.cs
@@ -2,35 +2,46 @@ using System;
 using LuxGarage.API.Data;
 using LuxGarage.API.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using Testcontainers.PostgreSql;
 
 namespace LuxGarage.Tests;
 
+[Collection("SharedDB")]
 public class VehicleRepositoryTestBase : IAsyncLifetime
 {
-    private readonly PostgreSqlContainer postgres = new PostgreSqlBuilder("postgres:17")
-        .WithDatabase("testDb")
-        .WithUsername("testuser")
-        .WithPassword("testpassword")
-        .Build();
     
+    private readonly SharedDatabaseFixture _fixture;
+
     protected RentalContext context { get; private set; } = null!;
 
+    private IDbContextTransaction _transaction = null!;
     protected int SeedBodyId { get; private set; }
     protected int SeedBrandId { get; private set; }
     protected int SeedColorId { get; private set; }
 
+    public VehicleRepositoryTestBase(SharedDatabaseFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
     public async Task InitializeAsync()
     {
-        await postgres.StartAsync();
 
         var options = new DbContextOptionsBuilder<RentalContext>()
-            .UseNpgsql(postgres.GetConnectionString())
+            .UseNpgsql(_fixture.DbContainer.GetConnectionString())
             .Options;
         
         context = new RentalContext(options);
-        await context.Database.MigrateAsync();
 
+        _transaction = await context.Database.BeginTransactionAsync();
+
+        await SeedDataBase();
+
+    }
+
+    private async Task SeedDataBase()
+    {
         var brand = new VehicleBrand { Id = 1, Name = "Mazda"};
         var body = new VehicleBody {Id = 1, Name = "Sedan"};
         var color = new VehicleColor { Id = 1, HtmlColor = "#FFFFFF", Name = "White" };
@@ -42,11 +53,16 @@ public class VehicleRepositoryTestBase : IAsyncLifetime
         SeedBodyId = body.Id;
         SeedBrandId = brand.Id;
         SeedColorId = color.Id;
+        await context.SaveChangesAsync();
+
     }
+
 
     public async Task DisposeAsync()
     {
+        // clean everything up by rolling back the transaction and disposing the context
+        await _transaction.RollbackAsync();
+        await _transaction.DisposeAsync();
         await context.DisposeAsync();
-        await postgres.DisposeAsync();
     }
 }
