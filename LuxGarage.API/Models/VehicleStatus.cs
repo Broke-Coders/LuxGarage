f9321959d9157ccb6773bd627844f2e7009bf5c8
Maciej Porębski <porebskimacio04@gmail.com>
2026-05-08 18:42:49 +0200

    Add offer and image models

--- LuxGarage.API/Models/VehicleStatus.cs
+++ LuxGarage.API/Models/VehicleStatus.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace LuxGarage.API.Models;
 public class VehicleStatus
 {
