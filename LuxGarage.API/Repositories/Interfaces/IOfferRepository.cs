cb0afa29315451c65c3e33ef4c036640b9e36209
Maciej Porębski <porebskimacio04@gmail.com>
2026-05-09 11:31:28 +0200

    Add offer repo interface

--- LuxGarage.API/Repositories/Interfaces/IOfferRepository.cs
+++ LuxGarage.API/Repositories/Interfaces/IOfferRepository.cs
@@ -1,8 +1,14 @@
-using System;
+using LuxGarage.API.Models;
 
 namespace LuxGarage.API.Repositories.Interfaces;
 
 public class IOfferRepository
 {
-
+    Task<List<Offer>> GetAllAsync();
+    Task<Offer?> GetByIdAsync(int id);
+    Task<Offer?> GetByVehicleIdAsync(int vehicleId);
+    Task AddAsync(Offer offer);
+    Task UpdateAsync(Offer offer);
+    Task DeleteAsync(Offer offer);
+    Task<bool> ExistsAsync(int id);
 }
