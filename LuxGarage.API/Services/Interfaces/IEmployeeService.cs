71ef7527f7812514fd1c18f5d0b073b7f3529d8f
Maciej Porębski <porebskimacio04@gmail.com>
2026-05-09 18:46:44 +0200

    Add vehicleImage service interface

--- LuxGarage.API/Services/Interfaces/IEmployeeService.cs
+++ LuxGarage.API/Services/Interfaces/IEmployeeService.cs
@@ -1,23 +1,21 @@
 ﻿using LuxGarage.API.DTOs.Requests;
 using LuxGarage.API.DTOs.Responses;
-using LuxGarage.API.Models;
 
-namespace LuxGarage.API.Services.Interfaces
+namespace LuxGarage.API.Services.Interfaces;
+
+/// <summary>
+/// Defines the interface for the employee service in the LuxGarage API, providing methods for managing employee data,
+/// including retrieval, updating, deletion, and password changes, while ensuring proper handling of 
+/// employee-related operations and returning appropriate responses for each action.
+/// </summary>
+public interface IEmployeeService
 {
-    /// <summary>
-    /// Defines the interface for the employee service in the LuxGarage API, providing methods for managing employee data,
-    /// including retrieval, updating, deletion, and password changes, while ensuring proper handling of 
-    /// employee-related operations and returning appropriate responses for each action.
-    /// </summary>
-    public interface IEmployeeService
-    {
-        Task<IEnumerable<EmployeeResponse>> GetAllAsync();
-        Task<EmployeeResponse?> GetByIdAsync(int id);
+    Task<IEnumerable<EmployeeResponse>> GetAllAsync();
+    Task<EmployeeResponse?> GetByIdAsync(int id);
 
-        Task<EmployeeResponse?> UpdateAsync(int id, UpdateEmployeeRequest request);
+    Task<EmployeeResponse?> UpdateAsync(int id, UpdateEmployeeRequest request);
 
-        Task<bool> DeleteAsync(int id);
+    Task<bool> DeleteAsync(int id);
 
-        Task<bool> ChangePasswordAsync(int id, ChangePasswordRequest request);
-    }
+    Task<bool> ChangePasswordAsync(int id, ChangePasswordRequest request);
 }
