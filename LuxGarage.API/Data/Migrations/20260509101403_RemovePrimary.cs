f2fc83e0bd143feeed6fdfe53040a283ac6784a3
Maciej Porębski <porebskimacio04@gmail.com>
2026-05-09 12:31:09 +0200

    Change configuration after removing isPrimary object in VehicleImages

---
+++ LuxGarage.API/Data/Migrations/20260509101403_RemovePrimary.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LuxGarage.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePrimary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPrimary",
+                table: "VehicleImages");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrimary",
+                table: "VehicleImages",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
