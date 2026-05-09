13be627280aeb22afece76fad97a9d709d8d9eb3
Maciej Porębski <porebskimacio04@gmail.com>
2026-05-09 20:04:57 +0200

    Add vehicleImage service implementation and add image response to autoMapper

--- LuxGarage.API/DTOs/VehicleImage/Responses/VehicleImageResponse.cs
+++ LuxGarage.API/DTOs/VehicleImage/Responses/VehicleImageResponse.cs
@@ -6,10 +6,9 @@ public class VehicleImageResponse
 {
     public int Id { get; set; }
     public int vehicleId { get; set; }
+    public string StorageKey { get; set; } = default!;
     public string OriginalFileName { get; set; } = default!;
     public string ContentType { get; set; } = default!;
     public long FileSize { get; set; }
     public int SortOrder { get; set; }
-    public string StorageKey { get; set; } = default!;
-    public string? Url { get; set; }
 }
--- LuxGarage.API/Profiles/MapperProfile.cs
+++ LuxGarage.API/Profiles/MapperProfile.cs
@@ -3,6 +3,8 @@ using LuxGarage.API.DTOs.Requests;
 using LuxGarage.API.DTOs.Requests.Vehicle;
 using LuxGarage.API.DTOs.Responses;
 using LuxGarage.API.DTOs.Responses.Vehicle;
+using LuxGarage.API.DTOs.VehicleImage.Requests;
+using LuxGarage.API.DTOs.VehicleImage.Responses;
 using LuxGarage.API.Models;
 
 namespace LuxGarage.API.Profiles
@@ -57,6 +59,9 @@ namespace LuxGarage.API.Profiles
 
             CreateMap<Workplace, WorkplaceResponse>();
             CreateMap<ChangeWorkplaceRequest, Workplace>();
+
+            // VehicleImage
+            CreateMap<VehicleImage, VehicleImageResponse>();
         }
     }
 }
