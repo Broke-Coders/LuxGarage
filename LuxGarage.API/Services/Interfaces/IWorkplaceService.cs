36e8d316b5dfe01e0b9a74ebb49924986445a9fa
Maciej Porębski <porebskimacio04@gmail.com>
2026-05-09 18:46:44 +0200

    Add vehicleImage service interface

--- LuxGarage.API/Services/Interfaces/IWorkplaceService.cs
+++ LuxGarage.API/Services/Interfaces/IWorkplaceService.cs
@@ -1,19 +1,18 @@
 ﻿using LuxGarage.API.DTOs.Requests;
 using LuxGarage.API.DTOs.Responses;
 
-namespace LuxGarage.API.Services.Interfaces
+namespace LuxGarage.API.Services.Interfaces;
+
+/// <summary>
+/// Defines the interface for the workplace service in the LuxGarage API, providing methods for managing workplace data,
+/// including retrieval, creation, updating, and deletion of workplaces, while ensuring proper validation
+/// for workplace data and returning appropriate responses for each action.
+/// </summary>
+public interface IWorkplaceService
 {
-    /// <summary>
-    /// Defines the interface for the workplace service in the LuxGarage API, providing methods for managing workplace data,
-    /// including retrieval, creation, updating, and deletion of workplaces, while ensuring proper validation
-    /// for workplace data and returning appropriate responses for each action.
-    /// </summary>
-    public interface IWorkplaceService
-    {
-        Task<IEnumerable<WorkplaceResponse>> GetAllAsync();
-        Task<WorkplaceResponse?> GetByIdAsync(int id);
-        Task<WorkplaceResponse> CreateAsync(ChangeWorkplaceRequest request);
-        Task<WorkplaceResponse?> UpdateAsync(ChangeWorkplaceRequest request, int id);
-        Task<bool> DeleteAsync(int id);
-    }
+    Task<IEnumerable<WorkplaceResponse>> GetAllAsync();
+    Task<WorkplaceResponse?> GetByIdAsync(int id);
+    Task<WorkplaceResponse> CreateAsync(ChangeWorkplaceRequest request);
+    Task<WorkplaceResponse?> UpdateAsync(ChangeWorkplaceRequest request, int id);
+    Task<bool> DeleteAsync(int id);
 }
