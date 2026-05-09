baa1af8c3f39e61060b91bc096b7c8ec03d2a0f6
Maciej Porębski <porebskimacio04@gmail.com>
2026-05-09 20:32:38 +0200

    Fix major issue with vulnerabilityin automapper and add nullable fields to clear warnings

--- LuxGarage.API/DTOs/Responses/RegisterResponse.cs
+++ LuxGarage.API/DTOs/Responses/RegisterResponse.cs
@@ -9,6 +9,6 @@ namespace LuxGarage.API.DTOs.Responses
     public class RegisterResponse
     {
         public int Id { get; set; }
-        public string Login { get; set; }
+        public string? Login { get; set; }
     }
 }
