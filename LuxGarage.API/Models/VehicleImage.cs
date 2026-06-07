41b3ce19431c2a6318d167e8c44304da2e9fd0d6
Maciej Porębski <porebskimacio04@gmail.com>
2026-06-08 00:09:54 +0200

    Change file name to OfferPrice for better understanding

--- LuxGarage.API/Models/VehicleImage.cs
+++ LuxGarage.API/Models/VehicleImage.cs
@@ -10,11 +10,12 @@ public class VehicleImage
 {
     public int Id { get; set; }
     public int VehicleId { get; set; }
-    public Vehicle Vehicle { get; set; } = default!;
+    public Vehicle Vehicle { get; set; } = null!;
 
-    public string StorageKey { get; set; } = default!;
-    public string OriginalFileName { get; set; } = default!;
-    public string ContentType { get; set; } = default!;
+    public string StorageKey { get; set; } = null!;
+    public string OriginalFileName { get; set; } = null!;
+    public string ContentType { get; set; } = null!;
     public long FileSize { get; set; }
     public int SortOrder { get; set; }
+    public bool IsPrimary { get; set; }
 }
