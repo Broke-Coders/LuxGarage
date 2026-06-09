24a80774b874a4a376e1a6625485018211c42d89
Maciej Porębski <porebskimacio04@gmail.com>
2026-06-09 13:03:39 +0200

    Delete all old repository, service and dto files - including folders

--- LuxGarage.API/DTOs/Offer/Requests/GetOffers.cs
+++
@@ -1,8 +0,0 @@
-namespace LuxGarage.API.DTOs.Requests.Offer;
-
-public class GetOffersRequest
-{
-    public string? SortBy { get; set; } = "id";
-    public bool Descending { get; set; }
-    public bool? OnlyAvailable { get; set; }
-}
