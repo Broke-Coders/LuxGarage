07dd486bc4b35c6f65fc98d39d0f5a0b13be94bc
Maciej Porębski <porebskimacio04@gmail.com>
2026-06-09 12:57:16 +0200

    Add automapper files for all new dtos

---
+++ LuxGarage.API/Features/Users/UserMapper.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using LuxGarage.API.Models; 
+
+namespace LuxGarage.API.Features.Users;
+
+public class UserMapper : Profile
+{
+    public UserMapper()
+    {
+        CreateMap<Customer, CustomerResponse>();
+
+        CreateMap<Employee, EmployeeResponse>()
+            .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role.ToString()))
+            .ForMember(dest => dest.WorkplaceName, opt => opt.MapFrom(src => 
+                src.Workplace != null ? $"{src.Workplace.City}, {src.Workplace.Street}" : null));
+    }
+}
