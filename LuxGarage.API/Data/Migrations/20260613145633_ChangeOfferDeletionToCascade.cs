d5f34e073d04fbdf7e7ad9e6a5fd75957ab930d1
Maciej Porębski <porebskimacio04@gmail.com>
2026-06-13 16:59:11 +0200

    Change offer deletion to cascade

---
+++ LuxGarage.API/Data/Migrations/20260613145633_ChangeOfferDeletionToCascade.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LuxGarage.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeOfferDeletionToCascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Offers_Vehicles_VehicleId",
+                table: "Offers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Offers_Vehicles_VehicleId",
+                table: "Offers",
+                column: "VehicleId",
+                principalTable: "Vehicles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Offers_Vehicles_VehicleId",
+                table: "Offers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Offers_Vehicles_VehicleId",
+                table: "Offers",
+                column: "VehicleId",
+                principalTable: "Vehicles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
